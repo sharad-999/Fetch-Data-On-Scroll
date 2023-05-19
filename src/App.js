a11724c82a73a160d9a841493b32ac97e5793367
Sharad Kevadiya <64027328+sharad-999@users.noreply.github.com>
2023-05-19 11:31:35 +0530

    Project completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
+  const [data, setdata] = useState([])
+  const [page, setpage] = useState(1);
+  const [totalData, settotalData] = useState(0);
+
+  useEffect(() => {
+    fetchData();
+     // eslint-disable-next-line
+  }, [])
+  const fetchData = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}`)
+      .then((res) => res.json())
+      .then((ele) =>{ 
+        setdata([...data,...ele]);
+        setpage(page+1);
+        console.log('ele.length', ele.length)
+        settotalData(100) //100 Total data
+        console.log('totaldata', totalData)
+      })
+      .catch((err)=>console.log('err', err))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchData}
+        hasMore={data.length<totalData}
+        loader={<h4>Loading...</h4>}
+      >
+        {
+          data.map((card) => (
+            <div className="card my-3" key={card.id}>
+              <div className="card-body" >
+                <h5 className="card-title">{card.id}</h5>
+                <h5 className="card-title">{card.title}</h5>
+                <p className="card-text">{card.body}</p>
+              </div>
+            </div>
+          ))
+        }
+      </InfiniteScroll>
+
     </div>
   );
 }
